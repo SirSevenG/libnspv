91e78d2b3e6d6109366f7fb799027abf2be0d859
SirSevenG <ottonseven@gmail.com>
2020-07-13 06:44:41 +0700

    use pycurl instead of requests

--- rpctest/test_nspv.py
+++ rpctest/test_nspv.py
@@ -19,7 +19,7 @@ import os
 
 
 def setup_module():
-    global addr_send, wif_real, coin, call, chain_params
+    global addr_send, wif_real, coin, call, chain_params, testlib
 
     wif_real = os.environ.get('WALL')
     addr_send = os.environ.get('ADDRESS')
@@ -76,37 +76,46 @@ def setup_module():
                     }
     userpass = "userpass"
     url = "http://127.0.0.1:" + chain_params.get(coin).get("port")
-    call = NRC(url, userpass)
-    call.nspv_logout()
+    call = NRC.proxy_connection(url)
+    testlib = NRC()
+    call.logout()
 
 
 def test_help_call():
     """ Response should contain "result": "success"
         Response should contain actual help data"""
     print('\n', "testing help call")
-    rpc_call = call.nspv_help()
+    rpc_call = call.help()
     if not rpc_call:
         pytest.exit("Can't connect daemon")
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "methods")
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "methods")
 
 
 def test_getpeerinfo_call():
     """Response should not be empty, at least one node should be in sync"""
     print('\n', "testing peerinfo call, checking peers status")
-    rpc_call = call.type_convert(call.nspv_getpeerinfo())
+    rpc_call = call.getpeerinfo()
     if not rpc_call[0]:
         raise Exception("Empty response :", rpc_call)
-    call.assert_contains(rpc_call[0], "ipaddress")
+    testlib.assert_contains(rpc_call[0], "ipaddress")
 
 
 def test_check_balance():
     """Check if wif given has actual balance to perform further tests"""
     print('\n', "Checking wif balance")
-    call.nspv_login(wif_real)
-    res = call.type_convert(call.nspv_listunspent())
-    amount = res.get("balance")
-    if amount < 0.1:
+    call.login(wif_real)
+    attempt = 0
+    while attempt < 10:  # after login, libnspv might take a few seconds to get user balance
+        res = call.listunspent()
+        amount = res.get("balance")
+        if amount < 0.1:
+            attempt += 1
+            print('Waiting for possible confirmations')
+            time.sleep(5)
+        else:
+            break
+    if attempt >= 10:
         pytest.exit("Not enough balance, please use another wif")
 
 
@@ -114,10 +123,10 @@ def test_getinfo_call():
     """ Response should contain "result": "success"
         Response should contain actual data"""
     print('\n', "testing getinfo call")
-    rpc_call = call.nspv_getinfo()
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "notarization")
-    call.assert_contains(rpc_call, "header")
+    rpc_call = call.getinfo()
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "notarization")
+    testlib.assert_contains(rpc_call, "header")
 
 
 def test_hdrsproof_call():
@@ -128,18 +137,17 @@ def test_hdrsproof_call():
     nextheight = [False, chain_params.get(coin).get("hdrs_proof_high")]
 
     # Case 1 - False data
-    rpc_call = call.nspv_hdrsproof(prevheight[0], nextheight[0])
-    call.assert_error(rpc_call)
+    rpc_call = call.hdrsproof(prevheight[0], nextheight[0])
+    testlib.assert_error(rpc_call)
 
     # Case 2 - known data
-    rpc_call = call.nspv_hdrsproof(prevheight[1], nextheight[1])
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "prevht")
-    call.assert_contains(rpc_call, "nextht")
-    call.assert_contains(rpc_call, "headers")
-    rep = call.type_convert(rpc_call)
-    hdrs_resp = rep.get('numhdrs')
-    call.assert_equal(hdrs_resp, chain_params.get(coin).get("numhdrs_expected"))
+    rpc_call = call.hdrsproof(prevheight[1], nextheight[1])
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "prevht")
+    testlib.assert_contains(rpc_call, "nextht")
+    testlib.assert_contains(rpc_call, "headers")
+    hdrs_resp = rpc_call.get('numhdrs')
+    testlib.assert_equal(hdrs_resp, chain_params.get(coin).get("numhdrs_expected"))
 
 
 def test_notarization_call():
@@ -149,24 +157,24 @@ def test_notarization_call():
     height = [False, chain_params.get(coin).get("notarization_height")]
 
     # Case 1 - False data
-    rpc_call = call.nspv_notarizations(height[0])
-    call.assert_error(rpc_call)
+    rpc_call = call.notarizations(height[0])
+    testlib.assert_error(rpc_call)
 
     # Case 2 - known data
-    rpc_call = call.nspv_notarizations(height[1])
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "prev")
-    call.assert_contains(rpc_call, "next")
+    rpc_call = call.notarizations(height[1])
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "prev")
+    testlib.assert_contains(rpc_call, "next")
 
 
 def getnewaddress_call():
     """ Get a new address, save it for latter calls"""
     print('\n', "testing getnewaddr call")
-    rpc_call = call.nspv_getnewaddress()
-    call.assert_contains(rpc_call, "wifprefix")
-    call.assert_contains(rpc_call, "wif")
-    call.assert_contains(rpc_call, "address")
-    call.assert_contains(rpc_call, "pubkey")
+    rpc_call = call.getnewaddress()
+    testlib.assert_contains(rpc_call, "wifprefix")
+    testlib.assert_contains(rpc_call, "wif")
+    testlib.assert_contains(rpc_call, "address")
+    testlib.assert_contains(rpc_call, "pubkey")
 
 
 def test_login_call():
@@ -174,16 +182,14 @@ def test_login_call():
         Response should contain address, address should be equal to generated earlier one"""
     print('\n', "testing log in call")
     global logged_address
-    rpc_call = call.nspv_getnewaddress()
-    rep = call.type_convert(rpc_call)
-    wif = rep.get('wif')
-    addr = rep.get('address')
-    rpc_call = call.nspv_login(wif)
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "status")
-    call.assert_contains(rpc_call, "address")
-    rep = call.type_convert(rpc_call)
-    logged_address = rep.get('address')
+    rpc_call = call.getnewaddress()
+    wif = rpc_call.get('wif')
+    addr = rpc_call.get('address')
+    rpc_call = call.login(wif)
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "status")
+    testlib.assert_contains(rpc_call, "address")
+    logged_address = rpc_call.get('address')
     if logged_address != addr:
         raise AssertionError("addr missmatch: ", addr, logged_address)
 
@@ -192,33 +198,30 @@ def test_listtransactions_call():
     """"Successful response should [not] contain txids and same address as requested
         Case 1 - False data, user is logged in - should not print txids for new address"""
     print('\n', "testing listtransactions call")
-    call.nspv_logout()
+    call.logout()
     real_addr = chain_params.get(coin).get("tx_list_address")
 
     # Case 1 - False Data
-    rpc_call = call.nspv_listtransactions(False, False, False)
-    call.assert_success(rpc_call)
-    call.assert_not_contains(rpc_call, "txids")
-    rep = call.type_convert(rpc_call)
-    addr_response = rep.get('address')
+    rpc_call = call.listtransactions(False, False, False)
+    testlib.assert_success(rpc_call)
+    testlib.assert_not_contains(rpc_call, "txids")
+    addr_response = rpc_call.get('address')
     if addr_response != logged_address:
         raise AssertionError("addr missmatch: ", addr_response, logged_address)
 
     # Case 2 - known data
-    rpc_call = call.nspv_listtransactions(real_addr, 0, 1)
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "txids")
-    rep = call.type_convert(rpc_call)
-    addr_response = rep.get('address')
+    rpc_call = call.listtransactions(real_addr, 0, 1)
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "txids")
+    addr_response = rpc_call.get('address')
     if addr_response != real_addr:
         raise AssertionError("addr missmatch: ", addr_response, real_addr)
 
     # Case 3 - known data, isCC = 1
-    rpc_call = call.nspv_listtransactions(real_addr, 1, 1)
-    call.assert_success(rpc_call)
-    call.assert_not_contains(rpc_call, "txids")
-    rep = call.type_convert(rpc_call)
-    addr_response = rep.get('address')
+    rpc_call = call.listtransactions(real_addr, 1, 1)
+    testlib.assert_success(rpc_call)
+    testlib.assert_not_contains(rpc_call, "txids")
+    addr_response = rpc_call.get('address')
     if addr_response != real_addr:
         raise AssertionError("addr missmatch: ", addr_response, real_addr)
 
@@ -226,33 +229,30 @@ def test_listtransactions_call():
 def test_litunspent_call():
     """ Successful response should [not] contain utxos and same address as requested"""
     print('\n', "testing listunspent call")
-    call.nspv_logout()
+    call.logout()
     real_addr = chain_params.get(coin).get("tx_list_address")
 
     # Case 1 - False dataf
-    rpc_call = call.nspv_listunspent(False, False, False)
-    call.assert_success(rpc_call)
-    call.assert_not_contains(rpc_call, "utxos")
-    rep = call.type_convert(rpc_call)
-    addr_response = rep.get('address')
+    rpc_call = call.listunspent(False, False, False)
+    testlib.assert_success(rpc_call)
+    testlib.assert_not_contains(rpc_call, "utxos")
+    addr_response = rpc_call.get('address')
     if addr_response != logged_address:
         raise AssertionError("addr missmatch: ", addr_response, logged_address)
 
     # Case 2 - known data
-    rpc_call = call.nspv_listunspent(real_addr, 0, 0)
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "utxos")
-    rep = call.type_convert(rpc_call)
-    addr_response = rep.get('address')
+    rpc_call = call.listunspent(real_addr, 0, 0)
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "utxos")
+    addr_response = rpc_call.get('address')
     if addr_response != real_addr:
         raise AssertionError("addr missmatch: ", addr_response, real_addr)
 
     # Case 3 - known data, isCC = 1, should not return utxos
-    rpc_call = call.nspv_listunspent(real_addr, 1, 0)
-    call.assert_success(rpc_call)
-    call.assert_not_contains(rpc_call, "utxos")
-    rep = call.type_convert(rpc_call)
-    addr_response = rep.get('address')
+    rpc_call = call.listunspent(real_addr, 1, 0)
+    testlib.assert_success(rpc_call)
+    testlib.assert_not_contains(rpc_call, "utxos")
+    addr_response = rpc_call.get('address')
     if addr_response != real_addr:
         raise AssertionError("addr missmatch: ", addr_response, real_addr)
 
@@ -264,49 +264,47 @@ def test_spend_call():
     address = [False, addr_send]
 
     # Case 1 - false data
-    rpc_call = call.nspv_spend(address[0], amount[0])
-    call.assert_error(rpc_call)
-    rpc_call = call.nspv_spend(address[1], amount[0])
-    call.assert_error(rpc_call)
+    rpc_call = call.spend(address[0], amount[0])
+    testlib.assert_error(rpc_call)
+    rpc_call = call.spend(address[1], amount[0])
+    testlib.assert_error(rpc_call)
 
     # Case 2 - known data, no legged in user
-    rpc_call = call.nspv_spend(address[1], amount[1])
-    call.assert_error(rpc_call)
+    rpc_call = call.spend(address[1], amount[1])
+    testlib.assert_error(rpc_call)
 
     # Case 3 - login with wif, create a valid transaction
-    call.nspv_logout()
-    call.nspv_login(wif_real)
-    rpc_call = call.nspv_spend(address[1], amount[1])
-    call.assert_success(rpc_call)
-    call.assert_contains(rpc_call, "tx")
-    call.assert_contains(rpc_call, "hex")
+    call.logout()
+    call.login(wif_real)
+    rpc_call = call.spend(address[1], amount[1])
+    testlib.assert_success(rpc_call)
+    testlib.assert_contains(rpc_call, "tx")
+    testlib.assert_contains(rpc_call, "hex")
 
 
 def test_broadcast_call():
     """Successful broadcasst should have equal hex broadcasted and expected"""
     print('\n', "testing broadcast call")
-    call.nspv_logout()
-    call.nspv_login(wif_real)
-    rpc_call = call.nspv_spend(addr_send, 0.1)
-    rep = call.type_convert(rpc_call)
-    hex_res = rep.get("hex")
+    call.logout()
+    call.login(wif_real)
+    rpc_call = call.spend(addr_send, 0.1)
+    hex_res = rpc_call.get("hex")
     hex = [False, "norealhexhere", hex_res]
     retcode_failed = [-1, -2, -3]
 
     # Cae 1 - No hex given
-    rpc_call = call.nspv_broadcast(hex[0])
-    call.assert_error(rpc_call)
+    rpc_call = call.broadcast(hex[0])
+    testlib.assert_error(rpc_call)
 
     # Case 2 - Non-valid hex, failed broadcast should contain appropriate retcode
-    rpc_call = call.nspv_broadcast(hex[1])
-    call.assert_in(rpc_call, "retcode", retcode_failed)
+    rpc_call = call.broadcast(hex[1])
+    testlib.assert_in(rpc_call, "retcode", retcode_failed)
 
     # Case 3 - Hex of previous transaction
-    rpc_call = call.nspv_broadcast(hex[2])
-    call.assert_success(rpc_call)
-    rep = call.type_convert(rpc_call)
-    broadcast_res = rep.get("broadcast")
-    expected = rep.get("expected")
+    rpc_call = call.broadcast(hex[2])
+    testlib.assert_success(rpc_call)
+    broadcast_res = rpc_call.get("broadcast")
+    expected = rpc_call.get("expected")
     if broadcast_res == expected:
         pass
     else:
@@ -316,8 +314,8 @@ def test_broadcast_call():
 def test_mempool_call():
     """ Response should contain txids"""
     print('\n', "testing mempool call")
-    rpc_call = call.nspv_mempool()
-    call.assert_success(rpc_call)
+    rpc_call = call.mempool()
+    testlib.assert_success(rpc_call)
     # call.assert_contains(rpc_call, "txids") - mempool() response not always contains "txids" key, even on success
 
 
@@ -328,17 +326,16 @@ def test_spentinfo_call():
     r_vouts = [False, 1]
 
     # Case 1 - False data
-    rpc_call = call.nspv_spentinfo(r_txids[0], r_vouts[0])
-    call.assert_error(rpc_call)
+    rpc_call = call.spentinfo(r_txids[0], r_vouts[0])
+    testlib.assert_error(rpc_call)
 
     # Case 2 - known data
-    rpc_call = call.nspv_spentinfo(r_txids[1], r_vouts[1])
-    call.assert_success(rpc_call)
-    rep = call.type_convert(rpc_call)
-    txid_resp = rep.get("txid")
+    rpc_call = call.spentinfo(r_txids[1], r_vouts[1])
+    testlib.assert_success(rpc_call)
+    txid_resp = rpc_call.get("txid")
     if r_txids[1] != txid_resp:
         raise AssertionError("Unexpected txid: ", r_txids[1], txid_resp)
-    vout_resp = rep.get("vout")
+    vout_resp = rpc_call.get("vout")
     if r_vouts[1] != vout_resp:
         raise AssertionError("Unxepected vout: ", r_vouts[1], vout_resp)
 
@@ -346,21 +343,20 @@ def test_spentinfo_call():
 def test_gettransaction():
     """Not implemented yet"""
     print('\n', "testing gettransaction call")
-    rpc_call = call.nspv_gettransaction()
+    rpc_call = call.gettransaction()
     call.assert_error(rpc_call)
 
 
 def test_autologout():
     """Wif should expeire in 777 seconds"""
     print('\n', "testing auto logout")
-    rpc_call = call.nspv_getnewaddress()
-    rep = call.type_convert(rpc_call)
-    wif = rep.get('wif')
-    rpc_call = call.nspv_login(wif)
-    call.assert_success(rpc_call)
+    rpc_call = call.getnewaddress()
+    wif = rpc_call.get('wif')
+    rpc_call = call.login(wif)
+    testlib.assert_success(rpc_call)
     time.sleep(778)
-    rpc_call = call.nspv_spend(addr_send, 0.1)
-    call.assert_error(rpc_call)
+    rpc_call = call.spend(addr_send, 0.1)
+    testlib.assert_error(rpc_call)
 
 
 def test_stop():
@@ -368,13 +364,13 @@ def test_stop():
        Stop nspv process after tests"""
     print('\n', "Resending funds")
     maxfee = 0.01
-    call.nspv_login(wif_real)
-    res = call.type_convert(call.nspv_listunspent())
+    call.login(wif_real)
+    res = call.listunspent()
     amount = res.get("balance") - maxfee
-    res = call.type_convert(call.nspv_spend(addr_send, amount))
+    res = call.spend(addr_send, amount)
     hexs = res.get("hex")
-    call.nspv_broadcast(hexs)
+    call.broadcast(hexs)
     print('\n', "stopping nspv process")
-    rpc_call = call.nspv_stop()
-    call.assert_success(rpc_call)
+    rpc_call = call.stop()
+    testlib.assert_success(rpc_call)
     print('\n', "all tests are finished")
